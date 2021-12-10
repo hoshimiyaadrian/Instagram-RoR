6a9be4996095d8e6e31ffe49307610b6fb453acb
hoshimiyaadrian <adriandh460@gmail.com>
2021-12-10 18:41:22 +0700

    finally part 2 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def show
+        @user  = User.find(params[:id])
+        @posts = @user.posts.order(created_at: :desc)
     end
 
     def edit
@@ -13,6 +15,6 @@ class UsersController < ApplicationController
     private
     def user_params
     params.require(:user).permit(:username, :name, :website,
-                                    :bio, :email, :phone, :gender)
+                                    :bio, :email, :phone, :gender, :avatar)
     end
 end
